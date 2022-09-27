70d1d97fbbf7629c63e0fc100f4d4a830d4fceb8
digiDreams <digital2dreamer@gmail.com>
2022-09-27 10:29:57 +0200

    First commit: initializing project

---
+++ parsingFromAPI/parsingFromAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  parsingFromAPIApp.swift
+//  parsingFromAPI
+//
+//  Created by Apprenant 16 on 27/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct parsingFromAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
